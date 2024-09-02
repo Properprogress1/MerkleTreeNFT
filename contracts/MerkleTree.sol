950a8aaa15facaf834e2ca8263f9f6296febcd5b
Tchisom <Kamsiyochukwu1@gmail..com>
2024-09-02 18:59:10 +0100

    doc: Resolve merge conflict

---
+++ contracts/MerkleTree.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract MaerkleTree {
+    using SafeERC20 for IERC20; 
+
+}
